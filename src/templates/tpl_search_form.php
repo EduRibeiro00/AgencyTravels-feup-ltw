780d0deb9c4931397c59d9f6580587be28ef3dd3
ManelCoutinho <manuel.monge.coutinho@gmail.com>
2019-11-23 12:35:27 +0000

    Added labels

--- src/templates/tpl_search_form.php
+++ src/templates/tpl_search_form.php
@@ -3,35 +3,47 @@
 		<fieldset>
 			<h4>Dates</h4>
 			<!-- TODO: mudar para current date mais 1 depois -->
-			<input type="date" name="checkin" min="2019-11-23">
-			<input type="date" name="checkout">
+			<label for="checkin">Check In</label>
+			<input type="date" id="checkin" name="checkin" min="2019-11-23">
+			<label for="checkout">Check Out</label>
+			<input type="date" id="checkout" name="checkout">
 		</fieldset>
 		<fieldset>
 			<h4>Guests</h4>
-			<input type="number" name="nAdults" min=0>
-			<input type="number" name="nChildren" min=0>
+			<label for="adults">Adults</label>
+			<input type="number" id="adults" name="nAdults" min=0 value=0>
+			<label for="children">Children</label>
+			<input type="number" id="children" name="nChildren" min=0 value=0>
 		</fieldset>
 		<fieldset>
 			<h4>Price</h4>
 			<!-- TODO: min < max -->
-			<input type="number" name="minPrice" min=0>
-			<input type="number" name="maxPrice" max=1000>
+			<label for="MinPrice">Min Price</label>
+			<input type="number" id="MinPrice" name="minPrice" value=0 min=0>
+			<label for="MaxPrice">Max Price</label>
+			<!-- TODO: por 1000 as 1000++ -->
+			<input type="number" id="MaxPrice" name="maxPrice" value=1000 max=1000>
 		</fieldset>
 		<fieldset>
 			<h4>Rating</h4>
-			<input type = "radio" name = "rating" valor = "5stars">
-			<input type = "radio" name = "rating" valor = "4stars">
-			<input type = "radio" name = "rating" valor = "3stars">
-			<input type = "radio" name = "rating" valor = "2stars">
-			<input type = "radio" name = "rating" valor = "1stars">
+			<input type = "radio" name = "rating" valor = "5stars" id = "5stars">
+			<label for="5stars"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i></label>
+			<input type = "radio" name = "rating" valor = "4stars" id = "4stars">
+			<label for="4stars"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i>+</label>
+			<input type = "radio" name = "rating" valor = "3stars" id = "3stars">
+			<label for="3stars"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>+</label>
+			<input type = "radio" name = "rating" valor = "2stars" id = "2stars">
+			<label for="2stars"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>+</label>
+			<input type = "radio" name = "rating" valor = "1stars" id = "1stars">
+			<label for="1stars"><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>+</label>
 		</fieldset>
 		<fieldset>
-			<h4>Rooms</h4>
-			<input type="number" name="nRooms" min=0>
+			<h4>Facilities</h4>
+			<label for="rooms">Rooms</label>
+			<input type="number" id="rooms" name="nRooms" min=0 value=0>
+			<label for="bathrooms">Bathooms</label>
+			<input type="number" id="bathrooms" name="nBathrooms" min=0 value=0>
 		</fieldset>
-		<fieldset>
-			<h4>Bathrooms</h4>
-			<input type="number" name="nBathrooms" min=0>
-		</fieldset>
+		<input type="submit" value="Search">
 	</form>
 <?php } ?>
