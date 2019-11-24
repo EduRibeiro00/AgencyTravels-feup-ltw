5aecd800807876bb090ca045817c92cd8a3c2960
EduRibeiro00 <eribeiro306@gmail.com>
2019-11-24 13:02:08 +0000

    Little changes

--- src/templates/tpl_house_card.php
+++ src/templates/tpl_house_card.php
@@ -1,13 +1,12 @@
 <!-- TODO: futuramente passar todos os house cards para aqui -->
 <!-- TODO: passar para parametro em vez de estático -->
 <?php function draw_horizontal_card() { ?>
-	<!-- TODO: ver se article, div  -->
 	<article class="row card">
 		<!-- TODO: mudar para carroussel -->
 		<a href="#"><img class="hcard-img" src="https://via.placeholder.com/460?text=Should+Be+Carroussel"></a>
-		<article class="column info">
+		<div class="column info">
 			<h4>Nome da Casa Muito fixe</h4>
-			<!-- TODO: ver isto, n pode ser 'p' pq depois n caem tipo spans -->
+			<!-- TODO: meter flexbox row, com divs ou spans -->
 			<p><span class="card-guests">5 guests</span><span class="card-bedroom">1 bedroom</span><span class="card-bathroom">1 bathroom</span></p>
 			<footer class="row">
 				<p>45€/noite</p>
@@ -16,6 +15,6 @@
 					(229)
 				</div>
 			</footer>
-		</article>
+		</div>
 	</article>
 <?php } ?>
