c4ff7950ba8df6ae1198b055c063f9b6935e3165
EduRibeiro00 <eribeiro306@gmail.com>
2019-11-27 23:58:44 +0000

    Missed a file

--- src/pages/list_places.php
+++ src/pages/list_places.php
@@ -3,10 +3,8 @@
 	include_once('../templates/tpl_common.php');
 	include_once('../templates/tpl_list_houses.php');
   
-	draw_head();
+	draw_head(['../js/main.js']);
 	draw_navbar();
-
 	list_houses_result();
-
 	draw_footer();
 ?>
