b17e0833f0a1cb8d765d46c6cd3b38b0b785d298
EduRibeiro00 <eribeiro306@gmail.com>
2019-11-27 16:58:50 +0000

    N sei

--- src/js/main.js
+++ src/js/main.js
@@ -113,7 +113,7 @@ function encodeForAjax(data) {
 	return Object.keys(data).map(function(k){
 	  return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
 	}).join('&')
-  }
+}
 
 //// Search and Suggestions
 // TODO: refactor disto [0]??
