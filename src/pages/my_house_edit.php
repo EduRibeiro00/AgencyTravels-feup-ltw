042c4df00d259c86e4fc4a2af2090835d7d11336
arubenruben <almeidaruben35@gmail.com>
2019-11-30 17:31:20 +0000

    First functional css my_house_edit

--- src/pages/my_house_edit.php
+++ src/pages/my_house_edit.php
@@ -7,46 +7,48 @@
     draw_navbar();
 
 
-    ?>
+?>
 
 
+<div id="my_house_edit_container">
 
-<form action="column" method="post">
+    <h2>My House Edit</h2>
 
-    <fieldset>
-        <legend>Description</legend>
-        <label>Title: <input type="text" name="title"> </label>
-    </fieldset>
+    <form action="column" method="post">
+        <fieldset>
+            <legend>Description</legend>
+            <label>Title: <input type="text" name="title"> </label>
+        </fieldset>
 
-    <fieldset>
-        <legend>Pictures</legend>
-        <!--Vai levar AJAX para apagar as photos-->
-        <!--Vai levar AJAX para caregar as novas photos uploaded-->
-        <article class="row">
-            
-            <img src="http://tap1.fkimg.com/media/vr-splice-j/05/a8/a5/30.jpg">
-            <img src="http://tap1.fkimg.com/media/vr-splice-j/05/a8/a5/30.jpg">
-            <img src="http://tap1.fkimg.com/media/vr-splice-j/05/a8/a5/30.jpg">
-            <input type="file" name="new_photo">
-            
-        </article>
+        <fieldset>
+            <legend>Pictures</legend>
+            <!--Vai levar AJAX para apagar as photos-->
+            <!--Vai levar AJAX para caregar as novas photos uploaded-->
+            <article class="row edit-house-imgs">
 
-    </fieldset>
-    
-    <fieldset>
-        <legend>Description</legend>
-        <textarea name="description" rows="10" cols="100">
+                <img src="http://tap1.fkimg.com/media/vr-splice-j/05/a8/a5/30.jpg">
+                <img src="http://tap1.fkimg.com/media/vr-splice-j/05/a8/a5/30.jpg">
+                <img src="http://tap1.fkimg.com/media/vr-splice-j/05/a8/a5/30.jpg">
+                <input type="file" name="new_photo">
+
+            </article>
+
+        </fieldset>
+
+        <fieldset>
+            <legend>Description</legend>
+            <textarea name="description" rows="10" cols="100">
 
             Preencher com PHP
         </textarea>
 
 
-    </fieldset>
+        </fieldset>
 
 
-    <fieldset>
-        <legend>Location</legend>
-            <article class="row">
+        <fieldset>
+            <legend>Location</legend>
+            <article class="row edit-house-location">
                 <label>Address: <input type="text" name="address"> </label>
                 <label>City: <input type="text" name="city"> </label>
                 <label>Country: <input type="text" name="country"> </label>
@@ -54,29 +56,23 @@
                 <label>GPS Coords: <input type="text" name="gps"> </label>
 
             </article>
-        
-
-        
-        </textarea>
 
 
-    </fieldset>
+        </fieldset>
 
+        <fieldset>
+            <legend>House Caracteristics</legend>
+            <article class="row edit-house-caracteristics">
+                <label>numRooms <input type="text" name="numRooms"></label>
+                <label>numBathrooms <input type="text" name="numBathrooms"></label>
+                <label>Capacity <input type="text" name="Capacity"></label>
+            </article>
+        </fieldset>
 
+        <input type="submit" value="Submit">
 
-
-
-    <input type="submit" value="Submit">
-
-</form>
-
-
-
-
-
-
-
-
+    </form>
+</div>
 
 <?php
     draw_footer();
