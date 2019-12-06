21cc7ffe3e4ff27dfc44f77d5c828e34e6915f88
EduRibeiro00 <eribeiro306@gmail.com>
2019-12-06 17:59:17 +0000

    Footer fixed

--- src/js/main.js
+++ src/js/main.js
@@ -21,6 +21,23 @@ window.addEventListener('scroll', function() {
 
 // -------------------
 
+//// Footer
+window.addEventListener('load', function() {
+	let html = document.documentElement;
+	let bodyHeight = html.scrollHeight - 30;
+
+	if(bodyHeight < screen.height) {
+		let footer = document.querySelector('body > footer');
+		footer.style.position = "fixed";
+		footer.style.bottom = "0";
+		footer.style.left = "0";
+		footer.style.right = "0";
+	}
+});
+
+
+// -------------------
+
 //// Search and Suggestions
 // TODO: refactor disto [0]??
 let locInput = document.getElementsByName("location")[0]
