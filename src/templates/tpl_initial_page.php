87dbf305a47749de171e8c87cc975690037618d6
ManelCoutinho <manuel.monge.coutinho@gmail.com>
2019-12-08 16:00:52 +0000

    Initial page clean up

--- src/templates/tpl_initial_page.php
+++ src/templates/tpl_initial_page.php
@@ -1,10 +1,5 @@
 <?php 
-include_once('../templates/tpl_reservation_utils.php');
-
-function draw_initial_page() { 
-			$ret = getPriceInDate(5, '2019-11-21', '2019-12-02');
-			echo $ret;
-			 ?>
+function draw_initial_page() { ?>
 	<div class="flex-column">
 		<div id="greetings">
 			<header id="initial">
