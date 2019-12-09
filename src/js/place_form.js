7d4f274d6c0bb807979a2df1481de7d262759b0f
arubenruben <almeidaruben35@gmail.com>
2019-12-09 21:50:58 +0000

    Imgs preview first commit working

--- src/js/place_form.js
+++ src/js/place_form.js
@@ -1,32 +1,38 @@
 'use strict'
 
 function encodeForAjax(data) {
-	return Object.keys(data).map(function(k){
-	  return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
-	}).join('&')
+    return Object.keys(data).map(function (k) {
+        return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+    }).join('&')
 }
 
 
 // photo upload
-const reader = new FileReader();
-let last_array_pos=0
 
+
+
+let last_array_pos = 0
 let profileFormImage = document.getElementById('img-to-upload');
 let imageInput = document.querySelector('input#imageFile_add_place');
 //In order to be possible to append childs
-let image_block_preview=document.querySelector('#img-upload div');
+let image_block_preview = document.querySelector('#img-upload div');
 
-imageInput.addEventListener('change', function(event) {
-    const f = event.target.files[0];
-    reader.readAsDataURL(f);
+imageInput.addEventListener('change', function (event) {
+
+    for (let i = 0; i < event.target.files.length; i++) {
+        let reader_inside = new FileReader();
+        let f = event.target.files[i];
+        reader_inside.readAsDataURL(f);
+        reader_inside.addEventListener('load', function (event) {
+            console.log("Aqui")
+            let image_to_append = document.createElement("img");
+            image_to_append.className = "edit_place_img_small";
+            image_to_append.src = event.target.result;
+            image_block_preview.appendChild(image_to_append);
+        });
+    }
 });
 
-reader.addEventListener('load', function(event) {
-    
-    let image_to_append=document.createElement("img");
-    image_to_append.src=event.target.result;
-    image_block_preview.appendChild(image_to_append);
-});
 
 // remove image button
 /*
