0d6f294e70be73ccbb9afaef6065640639b4ce18
EduRibeiro00 <eribeiro306@gmail.com>
2019-12-16 23:11:24 +0000

    Implemented several stuff about security

--- src/actions/action_logout.php
+++ src/actions/action_logout.php
@@ -2,7 +2,14 @@
     include_once('../includes/session_include.php');
 
     session_destroy();
+
+    session_set_cookie_params(0, '/', true, true);
     session_start();
+    session_regenerate_id(true);
+    
+    if (!isset($_SESSION['csrf'])) {
+        $_SESSION['csrf'] = generate_random_token();
+    }
 
     header('Location: ' . $_SERVER['HTTP_REFERER']);
 ?>
--- src/includes/session_include.php
+++ src/includes/session_include.php
@@ -1,3 +1,13 @@
 <?php
+    session_set_cookie_params(0, '/', true, true);
     session_start();
+    session_regenerate_id(true);
+
+    if (!isset($_SESSION['csrf'])) {
+        $_SESSION['csrf'] = generate_random_token();
+    }
+
+    function generate_random_token() {
+        return bin2hex(openssl_random_pseudo_bytes(32));
+    }
 ?>
