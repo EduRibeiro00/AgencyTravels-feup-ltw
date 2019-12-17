0ff0275d092b4f7b015341e53fc1fb0c95cb1a69
EduRibeiro00 <eribeiro306@gmail.com>
2019-12-17 10:34:23 +0000

    Security completely done

--- src/actions/action_logout.php
+++ src/actions/action_logout.php
@@ -3,7 +3,11 @@
 
     session_destroy();
 
-    // session_set_cookie_params(0, '/', true, true);
+    ini_set('session.cookie_httponly', 1);
+    // ini_set('session.cookie_secure', 1); // este ini_set da cookie_secure encontra-se comentado, uma vez que
+                                            // certas funcionalidades ligadas a sessao e a seus atributos
+                                            // (como por exemplo o login) deixavam de funcionar.
+
     session_start();
     session_regenerate_id(true);
     
